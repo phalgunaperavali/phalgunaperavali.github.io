145726697b5ab51fad3243ab9088e598a8827ff6
phalgunaperavali <phalgunaperavali333@gmail.com>
2025-02-13 21:44:06 -0500

    changes committed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
-/*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) => {
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+    nav = document.getElementById(navId)
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle', 'nav-menu')
+showMenu('nav-toggle','nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction() {
+function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
@@ -24,20 +24,20 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () => {
+const scrollActive = () =>{
     const scrollDown = window.scrollY
 
-    sections.forEach(current => {
+  sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 58,
-            sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
             sectionsClass.classList.add('active-link')
-        } else {
+        }else{
             sectionsClass.classList.remove('active-link')
-        }
+        }                                                    
     })
 }
 window.addEventListener('scroll', scrollActive)
@@ -48,14 +48,14 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-    //     reset: true
+//     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
-sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
-sr.reveal('.project__link, .project__img, .resume__container, .resume__description', { interval: 200 });
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.project__link, .project__img, .resume__container, .resume__description',{interval: 200});
 
 /* JavaScript to trigger animation when section enters viewport */
 document.addEventListener("DOMContentLoaded", function () {
@@ -83,3 +83,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", handleScroll);
 });
+
+
+
+function openModal(title, points) {
+    var modal = document.getElementById('workModal');
+    var modalTitle = document.getElementById('modalTitle');
+    var modalDescription = document.getElementById('modalDescription');
+
+    modalTitle.innerText = title;
+    modalDescription.innerHTML = ''; // Clear previous content
+    points.forEach(point => {
+        let li = document.createElement('li');
+        li.textContent = point;
+        modalDescription.appendChild(li);
+    });
+
+    modal.style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    var modal = document.getElementById('workModal');
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    var modal = document.getElementById('workModal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
